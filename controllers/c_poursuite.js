884527e283113dfc36d60384bc83138a5b64892a
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-04-23 23:11:15 +0200

    ajout alphabet de recherche
    à compléter car incomplet

--- controllers/c_poursuite.js
+++ controllers/c_poursuite.js
@@ -6,20 +6,43 @@ exports.affichage = function(request,response){
     let long = user.getLongueur();
 
     let french = require('an-array-of-french-words').filter(function(element){
+        if(element.includes('-')){
+            return false;
+        }
         if(element.length!=long){
             return false;
         }
         return !element.slice(0,lettre1.length).localeCompare(lettre1);
     });
 
-    console.log(french);
-    response.render('poursuite.ejs',{longueur:long,premiereLettre:lettre1,france:french});
+    //on créé une liste avec toutes les lettres de l'alphabet
+    //dès que l'on trouve l'une de ces lettres dans l'un des mots, on l'enlève de la liste
+    let optimizeAlphabet = "abcdefghijklmnopqrstuvwxyz";
+    french.forEach(element => {
+        for(let i=0;i<optimizeAlphabet.length;i++){
+            if(element.includes(optimizeAlphabet[i])){
+                console.log(optimizeAlphabet[i]);
+                optimizeAlphabet.replace(optimizeAlphabet[i],'');
+            }
+        }
+    });
+    console.log(optimizeAlphabet);
+
+    //on met ensuite à jour la liste des lettres possibles en enlevant de la liste des lettres Possibles toutes les 
+    //lettres étant resté dans la liste de vérification "optimizeAlphabet" car cela signifit qu'elles n'ont jamais 
+    //été utilisé dans la liste des bons mots donc ce ne sont pas des lettres possibles
+    for (let i = 0; i < user.getLettrePossible().length; i++) {
+        if(!optimizeAlphabet.includes(user.getLettrePossible()[i])){
+            user.RemoveLettrePossible(user.getLettrePossible()[i]);
+        }
+    }
+
+    response.render('poursuite.ejs',{longueur:long,premiereLettre:lettre1,france:french,lettrePossible:user.getLettrePossible()});
 }
 
 exports.calcul = function(request,response){
     console.log("sheeeesh");
 }
-
 
 
 /**
--- value/mot.js
+++ value/mot.js
@@ -3,13 +3,26 @@ let longueur = 0;
 let firstLettre = "";
 
 
+let lettrePossible = "abcdefghijklmnopqrstuvwxyz";
+
 module.exports = {
+    getLettrePossible(){
+        return lettrePossible;
+    },
+
     getLongueur(){
         return longueur;
     },
 
     getFirstLettre(){
         return firstLettre;
+    },
+
+    setLettrePossible(str){
+        lettrePossible = str;
+    },
+    RemoveLettrePossible(str){
+        lettrePossible.replace(str,'');
     },
     
     setLongueur(int){
