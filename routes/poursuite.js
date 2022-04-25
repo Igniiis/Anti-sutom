e5948a3860c78fb19816c530565766cabc84af1b
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-04-25 23:01:33 +0200

    Update poursuite.js

--- routes/poursuite.js
+++ routes/poursuite.js
@@ -5,9 +5,13 @@ const poursuite = require('../controllers/c_poursuite');
 //route de base
 router.get('/poursuite', poursuite.affichage);
 
-router.get('/poursuite/:mot',poursuite.affichagePrecis);
+router.get('/poursuite/:mot',poursuite.affichageP1);
 
 //   /poursuite/:mot/:lettreImpossibles/:lettrePresentes
+
+router.get('poursuite/:mot/:lettrePresente',poursuite.affichageP2);
+
+router.get('/poursuite/:mot/:lettrePresente/:lettreImpossible',poursuite.affichageP3);
 
 router.post('/poursuite',poursuite.calcul);
 
