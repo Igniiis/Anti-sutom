bf684dcbea8d1369575c8feca3fe4cc9274bfee3
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2022-05-22 21:51:49 +0200

    ajout mot le plus opti de la liste

--- controllers/c_poursuite.js
+++ controllers/c_poursuite.js
@@ -1,24 +1,26 @@
 let lLettres = "abcdefghijklmnopqrstuvwxyz";
-
+let motMax = "##erreur##";
+let tabMax = "";
+let voyelle = "aeiou";
 
 exports.affichageP1 = function (request,response) {
     let french = affichagePrecis(request.params.mot);
-    response.render("test.ejs",{france:french,mot:request.params.mot,lettrePresente:"",lettreImpossible:"",alphabet:lLettres});
+    response.render("test.ejs",{france:french,mot:request.params.mot,lettrePresente:"",lettreImpossible:"",alphabet:lLettres, motConceil: motMax});
 }
 
 exports.affichageP2 = function (request,response) {
     let french = affichagePrecis(request.params.mot, request.params.lettrePresente);
-    response.render("test.ejs",{france:french,mot:request.params.mot,lettrePresente:request.params.lettrePresente,lettreImpossible:"" ,alphabet:lLettres});
+    response.render("test.ejs",{france:french,mot:request.params.mot,lettrePresente:request.params.lettrePresente,lettreImpossible:"" ,alphabet:lLettres, motConceil: motMax});
 }
 
 exports.affichageP3 = function (request,response) {
     let french = affichagePrecis(request.params.mot, request.params.lettrePresente, request.params.lettreImpossible);
-    response.render("test.ejs",{france:french,mot:request.params.mot,lettrePresente:request.params.lettrePresente,lettreImpossible:request.params.lettreImpossible ,alphabet:lLettres});
+    response.render("test.ejs",{france:french,mot:request.params.mot,lettrePresente:request.params.lettrePresente,lettreImpossible:request.params.lettreImpossible ,alphabet:lLettres, motConceil: motMax});
 }
 
 exports.affichageP4 = function (request,response){
     let french = affichagePrecis(request.params.mot,"",request.params.lettreImpossible);
-    response.render("test.ejs",{france:french,mot:request.params.mot,lettrePresente:"",lettreImpossible:request.params.lettreImpossible ,alphabet:lLettres});
+    response.render("test.ejs",{france:french,mot:request.params.mot,lettrePresente:"",lettreImpossible:request.params.lettreImpossible ,alphabet:lLettres, motConceil: motMax});
 }
 
 
@@ -104,11 +106,30 @@ function affichagePrecis(mot,lettrePresente,lettreImpossible) {
                 return false;
             }
         }
+
+        let tabMaxPotentiel = "";
+
         for(let i=0;i<element.length;i++){
             if(!alphabet.includes(element[i])){
                 alphabet = alphabet+element[i];
             }
+
+            //partie optimisation pour trouver le mot Max
+
+            if (voyelle.includes(element[i])){ // si la lettre en question est une voyelle
+                if(!tabMaxPotentiel.includes(element[i])){ //si la liste des voyelles du mots ne possède toujours pas cette voyelle
+                    tabMaxPotentiel = tabMaxPotentiel + element[i]; //alors on l'ajoute
+                }
+            }
         }
+
+        if(tabMaxPotentiel.length>tabMax.length){{
+            tabMax = tabMaxPotentiel;
+            motMax = element;
+        }}
+
+
+        //return true si tout est bon
         return true;
     });
 
@@ -130,6 +151,32 @@ exports.ajoutLettre = function (request,response) {
     lettreImpossible = lettreImpossible + b;
 
     response.redirect(`/poursuite/${mot}/${lettrePresente}/${lettreImpossible}`);
+}
+
+function motOpti(listeMot) {
+    console.log(listeMot);
+    let voyelle = "aeiou";
+    let lettreUtile = "dflm";
+    let motMax = "";
+    let tabMax = [];
+    for (let i=0; i<listeMot.length; i++) {
+        console.log(listeMot[i]);
+        let tabLettre = [];
+        console.log(voyelle.length);
+        for(let j=0; i<voyelle.length;j++){
+            // console.log(voyelle[j]);
+            if(listeMot[i].includes(voyelle[j])){
+                tabLettre.push(voyelle[j]);
+            }
+        }
+        console.log(motMax + "___" + listeMot[i]);
+        if(tabMax.length<tabLettre.length){
+            motMax = listeMot[i];
+            tabMax = tabLettre;
+        }
+    }
+    console.log(motMax);
+    return motMax;
 }
 
 
